e8217a63dc97c7a7571834092b79dc876e1860ef
heart <7362469@qq.com>
2023-06-29 21:08:17 +0800

    style: remove unless code

--- scripts/shard.ts
+++ scripts/shard.ts
@@ -1,16 +1,12 @@
-import {glob} from 'glob'
+import { glob } from 'glob'
 import { resolve } from 'path'
 
-export function getMarkdownPath(filePath: string): Promise<Array<string>> {
-  return new Promise(async (res) => {
-
-    const path = resolve(process.cwd(), filePath, '**/*.{md,}')
-    try {
-      console.log(path);
-      const files = await glob(path)
-      res(files)
-    } catch(e) {
-      console.log('error', e);
-    }
-  })
+export async function getMarkdownPath(filePath: string): Promise<Array<string>> {
+  const path = resolve(process.cwd(), filePath, '**/*.{md,}')
+  try {
+    const files = await glob(path)
+    return files
+  } catch (e) {
+    console.log('error', e);
+  }
 }
